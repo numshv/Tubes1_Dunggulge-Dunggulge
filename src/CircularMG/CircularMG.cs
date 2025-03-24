7be9ac7e58cc678f850e151672c84a6b86b53c94
4clarissaNT4 <166925249+ClarissaNT44@users.noreply.github.com>
2025-03-24 13:03:00 +0700

    style : edit comment and description of the bot CircularMG

--- src/CircularMG/CircularMG.cs
+++ src/CircularMG/CircularMG.cs
@@ -5,16 +5,16 @@ using Robocode.TankRoyale.BotApi.Events;
 
 public class CircularMG : Bot
 {
-    // The main method starts our bot
+    // Main method untuk menjalankan bot
     static void Main(string[] args)
     {
         new CircularMG().Start();
     }
 
-    // Constructor, which loads the bot config file
+    // Konstruktor, yang memuat file konfigurasi bot
     CircularMG() : base(BotInfo.FromFile("CircularMG.json")) { }
 
-    // Called when a new round is started -> initialize and do some movement
+    // Melakukan inisialisasi pergerakan bot ketika permainan dimulai
     public override void Run()
     {
         BodyColor = Color.FromArgb(0xAD, 0xD8, 0xE6);   // Light Blue
@@ -26,27 +26,27 @@ public class CircularMG : Bot
         GunColor = Color.FromArgb(0x30, 0x19, 0x34);    // Dark Purple
         while (IsRunning)
         {
-            // Keep moving in a circle to scan for enemies and hard to target by enemies
+            // Selalu bergerak dalam lingkaran untuk mendeteksi musuh agar sulit dideteksi oleh musuh
             TurnGunLeft(360);
             TargetSpeed = 4;
             TurnRate = 5;
         }
     }
 
-    // Function for scanning the other bots
+    // Fungsi untuk mendeteksi musuh di sekitar bot
     public override void OnScannedBot(ScannedBotEvent e)
     {
-        // Determine the power of the shot based on the enemy's energy
+        // Menentukan tingkat kekuatan tembakan berdasarkan energi musuh
         double firePower;
-        if (e.Energy > 50) {
+        if (e.Energy > 50) { // Jika energi musuh di atas 50, gunakan firePower 1
             firePower = 1;
-        } else {
+        } else { // Jika energi musuh di bawah 50, gunakan firePower 3  
             firePower = 3;
         }
         Fire(firePower);
     }
 
-    // When the bot is hit by a bullet -> dodge
+    // Jika bot terkena dengan bot lain maka melakukan pergerakan dengan bot lain
     public override void OnHitBot(HitBotEvent e)
     {
         TargetSpeed = 4;
@@ -55,31 +55,31 @@ public class CircularMG : Bot
         Forward(100); 
     }
 
-    // When hit the wall -> turn back
+    // Jika bot terkena tembok maka melakukan pergerakan untuk mundur dan berputar
     public override void OnHitWall(HitWallEvent e)
     {
         Back(50);
         TurnRight(90);
-        for (int i = 0; i < 4; i++) { // Spinning to confuse the enemies
+        for (int i = 0; i < 4; i++) { // melanjutkan pola lingkaran untuk membingungkan musuh
             TargetSpeed = 5;
             TurnRate = 20;
         }
     }
 
-    // Saat terkena peluru -> menghindar
+    // Saat bot terkena tembakan dari musuh, maka bot akan melakukan pergerakan untuk menghindari tembakan
     public override void OnHitByBullet(HitByBulletEvent e)
     {
-        // Keep the information of the bullet direction to determine the movement
+        // Menyimpan informasi arah pergerakan peluru untuk menentukan pergerakan bot selanjutnya
         double bulletDirection = e.Bullet.Direction;
         if (bulletDirection > -90 && bulletDirection < 90)
         {
-            // If the bullet comes from the front, move to the right side
+            // Jika peluru datang dari depan, maka bot akan bergerak ke kanan
             TurnRight(90);
             Forward(150);
         }
         else
         {
-            // If the bullet comes from the back, move to the left side
+            // Jika peluru datang dari belakang, maka bot akan bergerak ke kiri
             TurnLeft(90);
             Forward(150);
         }
